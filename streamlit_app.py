04a251ffe587039ed3f335c7be106957e530e67a
RemiBounoua7 <remi.bounoua@telecom-sudparis.eu>
2025-02-28 11:15:26 +0100

    WaterMark

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,14 +126,14 @@ fig.update_layout(
     height=750,
     title=f"Contenders from {selected_date[0].day}/{selected_date[0].month}/{selected_date[0].year} to {selected_date[1].day}/{selected_date[1].month}/{selected_date[1].year}",
 )
-fig.add_annotation(x=.9, y=.9,
+fig.add_annotation(x=.01, y=.9,
             text="@ZieseI",
             showarrow=False,
             yshift=10,
             font=dict(
                 family="sans serif",
-                size=3,
-                color="#333232",
+                size=15,
+                color="#B7B7B7",
                 
                 )
             )
